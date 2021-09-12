1378f41c772576292e78051226f24593d7ae64ad
Mati <mati.halfa.92@gmail.com>
2021-09-12 21:02:53 +0300

    update server and read me

--- server/Server.js
+++ server/Server.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+const express = require('express')
+
 var dt = require('./myModule');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("My name is : " + dt.myName());
-  res.end();
-}).listen(8080);
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
--- server/myModule.js
+++ server/myModule.js
@@ -1,3 +1,22 @@
 exports.myName = function () {
     return "Mati Halfa";
   };
+
+  var events = require('events');
+  var eventEmitter = new events.EventEmitter();
+  
+  //Create an event handler:
+  var myEventHandler = function () {
+    console.log('I hear a scream!');
+  }
+  
+  //Assign the eventhandler to an event:
+  eventEmitter.on('scream', myEventHandler);
+  
+  exports.fireEvent = function () {
+    eventEmitter.emit('scream');
+  };
+
+  //Fire the 'scream' event:
+  
+
